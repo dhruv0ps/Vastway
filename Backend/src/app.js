848b44659ac0cbefc86ac529f8289b91f4e4e027
dhruv0ps <dhruv.dekhtawala2002@gmail.com>
2025-02-07 19:03:22 -0500

    just commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,9 +14,11 @@ app.get('/api/check', (req, res) => {
   res.status(200).send('Hello World!')
 })
 app.use("/api/", routes)
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Not Found" });
-});
+// app.use((req, res, next) => {
+//   let err = new Error("Not Found");
+//   err.status = 404
+//   next(err)
+// })
 
 init()
 server.listen(app.get('port'), () => {
